9286316532b78edde3e60002697d58b825126e43
James DV <azyriel14@gmail.com>
2016-05-27 09:39:32 +0800

    Streamline InputValidator code

--- InputValidator.java
+++ InputValidator.java
@@ -2,27 +2,8 @@ public class InputValidator {
 	
 	public InputValidator () {
 	}
-	
+
 	public boolean isValid(String inputString) {
-		boolean validity = false;
-		CheckNumberOfOccurences occurence = new CheckNumberOfOccurences(inputString, ",");
-		int resultingOccurence = occurence.countOccurences();
-		if (resultingOccurence == 1) {
-			String noWhiteSpaceInputString = inputString.replaceAll("\\s+","");
-			String [] parts = noWhiteSpaceInputString.split(",");
-			String part1 = parts[0];
-			String part2 = parts[1];
-			int part1Length = part1.length();
-			int part2Length = part2.length();
-			if ((part1Length != 0) && (part2Length != 0)) {
-				boolean part1LettersOnly = part1.matches("[a-zA-Z]+");
-				boolean part2LettersOnly = part2.matches("[a-zA-Z]+");
-				if(part1LettersOnly && part2LettersOnly) {
-					validity = true;
-				}
-			}
-		}
-		
-		return validity;
+		return inputString.matches("[a-zA-Z]+[a-zA-Z\\s]+[a-zA-Z]+,[a-zA-Z]+[a-zA-Z\\s]+[a-zA-Z]+");
 	}
 }
